5a62fc4cf87001e2eb9e7a42ccb75951e53e0246
Msksgm <msksgm3124@gmail.com>
2022-10-18 06:43:55 +0900

    first commit

---
+++ src/main/kotlin/com/example/openapigeneratorsample/OpenapiGeneratorSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.openapigeneratorsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenapiGeneratorSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenapiGeneratorSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/openapigeneratorsample/OpenapiGeneratorSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.openapigeneratorsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenapiGeneratorSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
